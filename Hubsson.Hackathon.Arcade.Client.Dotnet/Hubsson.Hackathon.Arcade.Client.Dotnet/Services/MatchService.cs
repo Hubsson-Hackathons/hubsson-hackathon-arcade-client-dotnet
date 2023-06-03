e71a3c2bcf2ccdbfd1992586fe8612d66a0e8492
Zoltán Nagy <zoltan0803@gmail.com>
2023-06-03 12:37:20 +0200

    feat(matchService): add boundary avoidance

--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Hubsson.Hackathon.Arcade.Client.Dotnet.Contracts;
 using ClientGameState = Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.ClientGameState;
 
 namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
@@ -7,11 +8,13 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
     {
         private MatchRepository _matchRepository;
         private ArcadeSettings _arcadeSettings;
-        
-        public MatchService(ArcadeSettings settings)
+        private readonly ILogger<MatchService> _logger;
+
+        public MatchService(ArcadeSettings settings, ILogger<MatchService> logger)
         {
             _matchRepository = new MatchRepository();
             _arcadeSettings = settings;
+            _logger = logger;
         }
         
         public void Init()
@@ -22,13 +25,121 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
 
         public Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.Action Update(ClientGameState gameState)
         {
-            // On Each Frame Update return an Action for your player
+            _logger.LogInformation($"{gameState}");
+            var action = new Domain.Action();
+            Coordinate coords = getMyCoordinates(gameState);
+            if (coords.x == 1)
+            {
+                // Bal széle
+                if (_matchRepository.currentDirection == Domain.Direction.Down || coords.y > gameState.height / 2)
+                {
+                    _logger.LogError("Turning up");
+                    _matchRepository.currentDirection = Domain.Direction.Up;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Up,
+                        iteration = gameState.iteration,
+                    };
+                }
+                else
+                {
+                    _logger.LogError("Turning down");
+                    _matchRepository.currentDirection = Domain.Direction.Down;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Down,
+                        iteration = gameState.iteration,
+                    };
+                }
+            }
+            if (coords.x == gameState.width - 1)
+            {
+                // jobb széle
+                if (_matchRepository.currentDirection == Domain.Direction.Down || coords.y > gameState.height / 2)
+                {
+                    _logger.LogError("Turning up");
+                    _matchRepository.currentDirection = Domain.Direction.Up;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Up,
+                        iteration = gameState.iteration,
+                    };
+                }
+                else
+                {
+                    _logger.LogError("Turning down");
+                    _matchRepository.currentDirection = Domain.Direction.Down;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Down,
+                        iteration = gameState.iteration,
+                    };
+                }
+            }
+            if (coords.y == 1)
+            {
+                // teteje
+                if (_matchRepository.currentDirection == Domain.Direction.Right || coords.x > gameState.width / 2)
+                {
+                    _logger.LogError("Turning left");
+                    _matchRepository.currentDirection = Domain.Direction.Left;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Left,
+                        iteration = gameState.iteration,
+                    };
+                }
+                else
+                {
+                    _logger.LogError("Turning right");
+                    _matchRepository.currentDirection = Domain.Direction.Right;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Right,
+                        iteration = gameState.iteration,
+                    };
+                }
+            }
+            if (coords.y == gameState.height - 1)
+            {
+                // alja
+                if (_matchRepository.currentDirection == Domain.Direction.Left || coords.x > gameState.width / 2)
+                {
+                    _logger.LogError("Turning left");
+                    _matchRepository.currentDirection = Domain.Direction.Left;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Left,
+                        iteration = gameState.iteration,
+                    };
+                }
+                else
+                {
+                    _logger.LogError("Turning right");
+                    _matchRepository.currentDirection = Domain.Direction.Right;
+                    return new Domain.Action
+                    {
+                        direction = Domain.Direction.Right,
+                        iteration = gameState.iteration,
+                    };
+                }
+            }
+            _matchRepository.currentDirection = Domain.Direction.Down;
+            return new Domain.Action
+            {
+                direction = Domain.Direction.Down,
+                iteration = gameState.iteration,
+            };
+        }
 
-            throw new NotImplementedException();
+        private Coordinate getMyCoordinates(ClientGameState gameState)
+        {
+            return gameState.players.FirstOrDefault(player => player.playerId == _arcadeSettings.TeamId).coordinates.Last();
         }
 
         private class MatchRepository
         {
+            public Domain.Direction currentDirection;
             // Write your data fields here what you would like to store between the match rounds
         }
     }
